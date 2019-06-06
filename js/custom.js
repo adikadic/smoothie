f565b1063202c6f7d8095b8adb240349853623ef
Adi <adi_kadic@hotmail.com>
2019-06-06 20:03:35 +0200

    Final version of website for Smoothie Bar.
    Needs to do some minor changes and delete unecessary files

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,11 @@ function closeNav() {
 
 (function($) {
 
+  var $totalPrice = 0;
+  var $waveHeight = 70;
+  var $counter = 0;
+
+
   // Menu filer
   $("#menu-flters li a").click(function() {
     $("#menu-flters li a").removeClass('active');
@@ -26,6 +31,73 @@ function closeNav() {
     }, 300);
   });
 
+  // on single ingredient click handle price calculation
+  $(".menu-restaurant").on("click", function() {
+    var $clickedItem = $(this);
+    var $clickedTitle = $(this).find(".menu-title");
+    var $clickedSubtitle = $(this).find('.menu-subtitle');
+    var $clickedPrice = $(this).find('.menu-price');
+
+    // use content on click and add to append
+    var $clickedTitleContent = $clickedTitle.text();
+    var $clickedSubtitleContent = $clickedSubtitle.text();
+
+    var $clickedPrice = parseFloat($clickedPrice.text().replace("$", ""));
+
+    var $hasClass = $clickedItem.hasClass("clicked");
+
+
+    if ($counter >= 0 && $counter <= 8) {
+
+      if ($hasClass) {
+        $clickedItem.removeClass("clicked");
+
+        // add values
+        $counter--;
+
+        // get the price and update global variable
+        $totalPrice -= $clickedPrice;
+        // set and animate
+        $('#smoothie-result-price > .price').html($totalPrice);
+        $waveHeight -= 40;
+        // animate glass
+
+        $('#water').css({
+          'backgroundPosition': "top-left",
+          height: $waveHeight + 'px'
+        })
+
+      } else {
+        $clickedItem.addClass("clicked");
+        // add values
+        $counter++;
+
+        // get the price and update global variable
+        $totalPrice += $clickedPrice;
+        // set and animate
+        $('#smoothie-result-price > .price').html($totalPrice);
+        $waveHeight += 40;
+        // animate glass
+
+        $('#water').css({
+          'backgroundPosition': "top-left",
+          height: $waveHeight + 'px'
+        })
+      }
+
+
+
+    }
+
+    $('#smoothie-result-price > .price').html($totalPrice);
+    $('#water').css({
+      'backgroundPosition': "top-left",
+      height: $waveHeight + 'px'
+    })
+
+  });
+
+
   // Add smooth scrolling to all links in navbar + footer link
   $(".sidenav a").on('click', function(event) {
     var hash = this.hash;
@@ -41,7 +113,7 @@ function closeNav() {
   });
 
   $(".sidenav a").on('click', function() {
-		closeNav();
-	});
+    closeNav();
+  });
 
 })(jQuery);
